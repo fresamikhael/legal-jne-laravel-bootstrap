4b3b8739fadd32a0dbfcfc37e8dcf6bf471a8561
fresaletlora <mikhaelfresa@gmail.com>
2022-04-12 13:28:46 +0700

    update

---
+++ resources/views/includes/user/navbar.blade.php
@@ -0,0 +1,40 @@
+<!-- ***** Header Area Start ***** -->
+<header class="header-area header-sticky wow slideInDown" data-wow-duration="0.75s" data-wow-delay="0s">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <!-- ***** Logo Start ***** -->
+                    <a href="index.html" class="logo">
+                        <img src="/images/logo.png" alt="" class="logo" />
+                    </a>
+                    <!-- ***** Logo End ***** -->
+                    <!-- ***** Menu Start ***** -->
+                    <ul class="nav">
+                        <li class="scroll-to-section"><a href="#about">Regulasi</a></li>
+                        <li class="scroll-to-section">
+                            <a href="#services">Statistik Pekerjaan</a>
+                        </li>
+                        <li class="scroll-to-section">
+                            <a href="#portfolio">Informasi</a>
+                        </li>
+                        <li class="scroll-to-section"><a href="#blog">Database</a></li>
+                        <li class="scroll-to-section">
+                            <a href="#contact">Hubungi Kami</a>
+                        </li>
+                        <li class="scroll-to-section">
+                            <div class="main-red-button">
+                                <a href="/login.html">Login</a>
+                            </div>
+                        </li>
+                    </ul>
+                    <a class="menu-trigger">
+                        <span>Menu</span>
+                    </a>
+                    <!-- ***** Menu End ***** -->
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- ***** Header Area End ***** -->
---
+++ resources/views/includes/user/style.blade.php
@@ -0,0 +1,8 @@
+<!-- Bootstrap core CSS -->
+<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+
+<!-- Additional CSS Files -->
+<link rel="stylesheet" href="/css/fontawesome.css" />
+<link rel="stylesheet" href="/css/app.css" />
+<link rel="stylesheet" href="/css/animated.css" />
+<link rel="stylesheet" href="/css/owl.css" />
