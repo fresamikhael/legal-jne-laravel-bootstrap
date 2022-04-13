a155ceb869e99a40bf28433bb86fb2acdd120def
fresaletlora <mikhaelfresa@gmail.com>
2022-04-13 11:01:52 +0700

    update

--- resources/views/pages/user/index.blade.php
+++ resources/views/pages/user/index.blade.php
@@ -5,13 +5,13 @@
 @endsection
 
 @section('content')
-    <x-organisms.banner></x-organisms.banner>
+    <x-organisms.banner />
 
-    <x-organisms.about></x-organisms.about>
+    <x-organisms.about />
 
-    <x-organisms.service></x-organisms.service>
+    <x-organisms.service />
 
-    <x-organisms.portofolio></x-organisms.portofolio>
+    <x-organisms.portofolio />
 
-    <x-organisms.contact></x-organisms.contact>
+    <x-organisms.contact />
 @endsection
