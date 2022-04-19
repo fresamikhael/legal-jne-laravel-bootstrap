d502fa2831576d6ee8e6aafef27ac0b83a8087f4
MIBR47 <ilhambachtiar47@gmail.com>
2022-04-19 12:37:44 +0700

    upload new permit dan input tambah name

--- app/View/Components/Input.php
+++ app/View/Components/Input.php
@@ -12,6 +12,7 @@ class Input extends Component
     public $label;
     public $type;
     public $name;
+    public $id;
     public $placeholder;
     public $disabled;
     public $required;
@@ -24,25 +25,26 @@ class Input extends Component
      * @return void
      */
     public function __construct(
-        $inputClass = null, 
-        $labelClass = null, 
-        $fieldClass = null, 
-        $label = null, 
-        $type = null, 
-        $name = null, 
-        $placeholder = null, 
-        $disabled = null, 
+        $inputClass = null,
+        $labelClass = null,
+        $fieldClass = null,
+        $label = null,
+        $type = null,
+        $name = null,
+        $id = null,
+        $placeholder = null,
+        $disabled = null,
         $required = null,
         $prefix = null,
         $hidden = null,
-    )
-    {
+    ) {
         $this->inputClass = $inputClass;
         $this->labelClass = $labelClass;
         $this->fieldClass = $fieldClass;
         $this->label = $label;
         $this->type = $type;
         $this->name = $name;
+        $this->id = $id;
         $this->placeholder = $placeholder;
         $this->disabled = $disabled;
         $this->required = $required;
--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,16 +1,19 @@
-<div class="mb-3 row {{ $inputClass }}" id="{{ $name . 1 }}" style="{{ $hidden ? "display: none !important;" : "" }}">
+<div class="mb-3 row {{ $inputClass }}" id="{{ $name . 1 }}"
+    style="{{ $hidden ? 'display: none !important;' : '' }}">
     <label for="{{ $name }}"
         class="{{ $labelClass ? $labelClass : 'col-sm-3' }} col-form-label">{{ $label }}</label>
     <div class="{{ $fieldClass ? $fieldClass : 'col-sm-9' }}">
         @if ($prefix)
             <div class="input-group">
                 <span class="input-group-text">{{ $prefix }}</span>
-                <input type="{{ $type ? $type : "text" }}" class="form-control" id="{{ $name }}" 
-                    placeholder="{{ $placeholder }}" {{ $disabled ? 'disabled' : '' }} {{ $required ? 'required' : '' }}>
+                <input type="{{ $type ? $type : 'text' }}" class="form-control" id="{{ $id }}"
+                    name={{ $name }} placeholder="{{ $placeholder }}" {{ $disabled ? 'disabled' : '' }}
+                    {{ $required ? 'required' : '' }}>
             </div>
         @else
-            <input type="{{ $type ? $type : "text" }}" class="form-control" id="{{ $name }}"
-                placeholder="{{ $placeholder }}" {{ $disabled ? 'disabled' : '' }} {{ $required ? 'required' : '' }}>
+            <input type="{{ $type ? $type : 'text' }}" class="form-control" id="{{ $name }}"
+                name={{ $name }} placeholder="{{ $placeholder }}" {{ $disabled ? 'disabled' : '' }}
+                {{ $required ? 'required' : '' }}>
         @endif
     </div>
 </div>
