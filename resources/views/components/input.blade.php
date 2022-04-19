f64d7aa3ac1c175419e61826c958f27e7b30e715
MIBR47 <ilhambachtiar47@gmail.com>
2022-04-19 13:56:54 +0700

    upload new permit dan input tambah name 3

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -6,7 +6,6 @@
         @if ($prefix)
             <div class="input-group">
                 <span class="input-group-text">{{ $prefix }}</span>
-<<<<<<< HEAD
                 <input type="{{ $type ? $type : 'text' }}" class="form-control" id="{{ $id }}"
                     name={{ $name }} placeholder="{{ $placeholder }}" {{ $disabled ? 'disabled' : '' }}
                     {{ $required ? 'required' : '' }}>
@@ -14,22 +13,6 @@
         @else
             <input type="{{ $type ? $type : 'text' }}" class="form-control" id="{{ $name }}"
                 name={{ $name }} placeholder="{{ $placeholder }}" {{ $disabled ? 'disabled' : '' }}
-=======
-                <input type="{{ $type ? $type : 'text' }}" class="form-control" id="{{ $name }}"
-                    placeholder="{{ $placeholder }}" {{ $disabled ? 'disabled' : '' }}
-                    {{ $required ? 'required' : '' }}>
-            </div>
-        @elseif ($postfix)
-            <div class="input-group">
-                <input type="{{ $type ? $type : 'text' }}" class="form-control" id="{{ $name }}"
-                    placeholder="{{ $placeholder }}" {{ $disabled ? 'disabled' : '' }}
-                    {{ $required ? 'required' : '' }}>
-                <span class="input-group-text">{{ $postfix }}</span>
-            </div>
-        @else
-            <input type="{{ $type ? $type : 'text' }}" class="form-control" id="{{ $name }}"
-                placeholder="{{ $placeholder }}" {{ $disabled ? 'disabled' : '' }}
->>>>>>> a441ae0cb4a22bd7946605a847a69b51e6378993
                 {{ $required ? 'required' : '' }}>
         @endif
     </div>
