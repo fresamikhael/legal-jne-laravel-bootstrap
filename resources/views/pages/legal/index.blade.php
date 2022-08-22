0f925577d5610654e10e3c28df428c07a365df43
fresaletlora <mikhaelfresa@gmail.com>
2022-08-22 14:11:14 +0700

    update

--- resources/views/pages/legal/index.blade.php
+++ resources/views/pages/legal/index.blade.php
@@ -87,13 +87,13 @@
                     <div class="row">
                         <div class="col-lg-6 align-self-center">
                             <div class="left-content header-text wow fadeInLeft" data-wow-duration="1s" data-wow-delay="1s">
-                                <h6>{{ $top->t1 }}</h6>
+                                <h6>{{ $top->t1 ?? '' }}</h6>
                                 <h2>
 
-                                    <em>{{ $top->t2 }}</em>
+                                    <em>{{ $top->t2 ?? '' }}</em>
                                 </h2>
                                 <p>
-                                    {{ $top->t3 }}
+                                    {{ $top->t3 ?? '' }}
                                 </p>
                                 <a href="#services" class="btn btn-danger">Pengajuan <i class="fas fa-arrow-right"></i></a>
                                 <a href="{{ route('top-home') }}" class="btn btn-danger">Ubah Halaman Ini <i
@@ -102,7 +102,7 @@
                         </div>
                         <div class="col-lg-6">
                             <div class="right-image wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
-                                <img src="{{ asset($top->photo) }}" alt="team meeting" />
+                                <img src="{{ asset($top->photo ?? '') }}" alt="team meeting" />
                             </div>
                         </div>
                     </div>
@@ -135,7 +135,7 @@
                         <div data-wow-duration="1s" data-wow-delay="0.3s">
                             <div class="hidden-content">
                                 <h4>Drafting</h4>
-                                <p>{{ $foot->t1 }}</p>
+                                <p>{{ $foot->t1 ?? '' }}</p>
                             </div>
                             <div class="showed-content">
                                 <img src="{{ url('/images/hero.png') }}" alt="" />
@@ -149,7 +149,7 @@
                         <div data-wow-duration="1s" data-wow-delay="0.4s">
                             <div class="hidden-content">
                                 <h4>Litigation</h4>
-                                <p>{{ $foot->t2 }}</p>
+                                <p>{{ $foot->t2 ?? '' }}</p>
                             </div>
                             <div class="showed-content">
                                 <img src="{{ url('/images/hero.png') }}" alt="" />
@@ -163,7 +163,7 @@
                         <div data-wow-duration="1s" data-wow-delay="0.5s">
                             <div class="hidden-content">
                                 <h4>Permit</h4>
-                                <p>{{ $foot->t3 }}</p>
+                                <p>{{ $foot->t3 ?? '' }}</p>
                             </div>
                             <div class="showed-content">
                                 <img src="{{ url('/images/hero.png') }}" alt="" />
@@ -177,7 +177,7 @@
                         <div data-wow-duration="1s" data-wow-delay="0.6s">
                             <div class="hidden-content">
                                 <h4>Legal Corporate</h4>
-                                <p>{{ $foot->t4 }}</p>
+                                <p>{{ $foot->t4 ?? '' }}</p>
                             </div>
                             <div class="showed-content">
                                 <img src="{{ url('/images/hero.png') }}" alt="" />
@@ -191,7 +191,7 @@
                         <div data-wow-duration="1s" data-wow-delay="0.6s">
                             <div class="hidden-content">
                                 <h4>Permohonan Dokumen</h4>
-                                <p>{{ $foot->t5 }}</p>
+                                <p>{{ $foot->t5 ?? '' }}</p>
                             </div>
                             <div class="showed-content">
                                 <img src="{{ url('/images/hero.png') }}" alt="" />
@@ -209,7 +209,7 @@
             <div class="row">
                 <div class="col-lg-4">
                     <div class="left-image wow fadeIn" data-wow-duration="1s" data-wow-delay="0.2s">
-                        <img src="{{ asset($middle->photo) }}" style="margin-top: -50px" alt="person graphic" />
+                        <img src="{{ asset($middle->photo ?? '') }}" style="margin-top: -50px" alt="person graphic" />
                     </div>
                     <a href="{{ route('middle-home') }}" class="btn btn-danger">Ubah Halaman Ini <i
                             class="fas fa-edit"></i></a>
@@ -220,12 +220,12 @@
                             <div class="col-lg-6">
                                 <div class="item wow fadeIn" data-wow-duration="1s" data-wow-delay="0.5s">
                                     <div class="icon">
-                                        <img src="{{ asset($middle->p1) }}" alt="reporting" />
+                                        <img src="{{ asset($middle->p1 ?? '') }}" alt="reporting" />
                                     </div>
                                     <div class="right-text">
                                         <h4>Drafting</h4>
                                         <p>
-                                            {{ $middle->t1 }}
+                                            {{ $middle->t1 ?? '' }}
                                         </p>
                                     </div>
                                 </div>
@@ -233,12 +233,12 @@
                             <div class="col-lg-6">
                                 <div class="item wow fadeIn" data-wow-duration="1s" data-wow-delay="0.7s">
                                     <div class="icon">
-                                        <img src="{{ asset($middle->p2) }}" alt="" />
+                                        <img src="{{ asset($middle->p2 ?? '') }}" alt="" />
                                     </div>
                                     <div class="right-text">
                                         <h4>Litigation</h4>
                                         <p>
-                                            {{ $middle->t2 }}
+                                            {{ $middle->t2 ?? '' }}
                                         </p>
                                     </div>
                                 </div>
@@ -246,12 +246,12 @@
                             <div class="col-lg-6">
                                 <div class="item wow fadeIn" data-wow-duration="1s" data-wow-delay="0.9s">
                                     <div class="icon">
-                                        <img src="{{ asset($middle->p3) }}" alt="" />
+                                        <img src="{{ asset($middle->p3 ?? '') }}" alt="" />
                                     </div>
                                     <div class="right-text">
                                         <h4>Permit</h4>
                                         <p>
-                                            {{ $middle->t3 }}
+                                            {{ $middle->t3 ?? '' }}
                                         </p>
                                     </div>
                                 </div>
@@ -259,12 +259,12 @@
                             <div class="col-lg-6">
                                 <div class="item wow fadeIn" data-wow-duration="1s" data-wow-delay="1.1s">
                                     <div class="icon">
-                                        <img src="{{ asset($middle->p4) }}" alt="" />
+                                        <img src="{{ asset($middle->p4 ?? '') }}" alt="" />
                                     </div>
                                     <div class="right-text">
                                         <h4>Legal Corporate</h4>
                                         <p>
-                                            {{ $middle->t4 }}
+                                            {{ $middle->t4 ?? '' }}
                                         </p>
                                     </div>
                                 </div>
@@ -272,12 +272,12 @@
                             <div class="col-lg-6">
                                 <div class="item wow fadeIn" data-wow-duration="1s" data-wow-delay="1.1s">
                                     <div class="icon">
-                                        <img src="{{ asset($middle->p5) }}" alt="" />
+                                        <img src="{{ asset($middle->p5 ?? '') }}" alt="" />
                                     </div>
                                     <div class="right-text">
                                         <h4>Permohonan Berkas</h4>
                                         <p>
-                                            {{ $middle->t5 }}
+                                            {{ $middle->t5 ?? '' }}
                                         </p>
                                     </div>
                                 </div>
@@ -294,7 +294,7 @@
             <div class="row">
                 <div class="col-lg-6 align-self-center wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.2s">
                     <div class="left-image">
-                        <img src="{{ asset($below->photo) }}" style="margin-left: 100px; margin-top: -50px"
+                        <img src="{{ asset($below->photo ?? '') }}" style="margin-left: 100px; margin-top: -50px"
                             class="w-50" alt="" />
                     </div>
                     <a href="{{ route('below-home') }}" class="btn btn-danger">Ubah Halaman Ini <i
@@ -303,10 +303,10 @@
                 <div class="col-lg-6 wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.2s">
                     <div class="section-heading">
                         <h2>
-                            {{ $below->t1 }}
+                            {{ $below->t1 ?? '' }}
                         </h2>
                         <p>
-                            {{ $below->t2 }}
+                            {{ $below->t2 ?? '' }}
                         </p>
                     </div>
                     <div class="row">
